2edc12942d3f1c1d31ea6a700d7bad8a2a36e975
Daniel Cañada García <d4nicoder@gmail.com>
2021-11-18 00:28:49 +0100

    fix: reviewers filtering

--- src/interactors/getReviewers/index.js
+++ src/interactors/getReviewers/index.js
@@ -9,4 +9,4 @@ module.exports = (pulls) => groupReviews(pulls).map(({ author, reviews }) => {
   const stats = calculateReviewsStats(reviews);
   console.log(author)
   return { author, reviews, stats };
-}).filter(info => reviewers.length && reviewers.includes(info.author.login));
+}).filter(info => !reviewers.length || reviewers.includes(info.author.login));
