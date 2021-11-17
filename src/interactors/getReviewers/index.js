e37a399b9c00f5570fe3000e8e1524e232494639
Daniel Cañada García <d4nicoder@gmail.com>
2021-11-18 00:54:06 +0100

    WIP

--- src/interactors/getReviewers/index.js
+++ src/interactors/getReviewers/index.js
@@ -3,11 +3,7 @@ const groupReviews = require('./groupReviews');
 const core = require('@actions/core');
 const reviewers = core.getInput('reviewers').split(',').filter(reviewer => reviewer.trim() !== '')
 
-console.log(typeof reviewers)
-console.log(reviewers)
-
 module.exports = (pulls) => groupReviews(pulls).map(({ author, reviews }) => {
   const stats = calculateReviewsStats(reviews);
-  console.log(author)
   return { author, reviews, stats };
 }).filter(info => reviewers.length === 0 || reviewers.includes(info.author.login));
