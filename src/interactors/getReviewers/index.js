39dfc47f2fd93109e5268e808ce9734ae71743cf
Daniel Cañada García <d4nicoder@gmail.com>
2021-11-18 00:50:59 +0100

    wip

--- src/interactors/getReviewers/index.js
+++ src/interactors/getReviewers/index.js
@@ -10,4 +10,4 @@ module.exports = (pulls) => groupReviews(pulls).map(({ author, reviews }) => {
   const stats = calculateReviewsStats(reviews);
   console.log(author)
   return { author, reviews, stats };
-}).filter(info => !reviewers.length || reviewers.includes(info.author.login));
+}).filter(info => reviewers.length === 0 || reviewers.includes(info.author.login));
