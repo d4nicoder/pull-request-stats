1a218f87c84e576665d73b827e457cbd27583a91
Daniel Cañada García <d4nicoder@gmail.com>
2021-11-18 01:17:40 +0100

    wip

--- src/interactors/getReviewers/index.js
+++ src/interactors/getReviewers/index.js
@@ -1,9 +1,21 @@
 const calculateReviewsStats = require('./calculateReviewsStats');
 const groupReviews = require('./groupReviews');
 const core = require('@actions/core');
-const reviewers = core.getInput('reviewers').split(',').filter(reviewer => reviewer.trim() !== '')
+const reviewers = core.getInput('reviewers').split(',').filter(reviewer => reviewer.trim() !== '').map(reviewer => reviewer.trim())
+
+core.info(reviewers)
 
 module.exports = (pulls) => groupReviews(pulls).map(({ author, reviews }) => {
   const stats = calculateReviewsStats(reviews);
+  core.info(author)
   return { author, reviews, stats };
-}).filter(info => reviewers.length === 0 || reviewers.includes(info.author.login));
+}).filter(info => {
+  if (!reviewers.length) {
+    core.info(`Not filtering by reviewers`)
+    return true
+  } else if (reviewers.includes(info.author.login)) {
+    core.info(`Author ${info.author.login} is present`)
+    return true
+  }
+  return false
+});
